95bf5e1203cbad2854fb115a3f5fb3cecbfd1fb6
Puranjay Mohan <puranjay@kernel.org>
2026-02-18 06:14:39 -0800

    Modify to support building cilium programs
    This commit modifies the gcc bpf build action into a similar action that
    builds and releases bpf programs from cilium
    
    Signed-off-by: Puranjay Mohan <puranjay@kernel.org>

--- update-latest-release.sh
+++ update-latest-release.sh
@@ -2,26 +2,25 @@
 
 set -euo pipefail
 
-GCC_BPF_INSTALLDIR=$1
+BPF_INSTALLDIR=$1
 
-latest_release_tag=$(gh release list -L 1 -R theihor/gcc-bpf --json tagName -q .[].tagName)
+latest_release_tag=$(gh release list -L 1 --json tagName -q .[].tagName)
 
-binutils_version=$(ls "${GCC_BPF_INSTALLDIR}" | grep "binutils.*built" | sed 's/.built//')
-gcc_version=$(ls "${GCC_BPF_INSTALLDIR}" | grep "gcc.*built" | sed 's/.built//')
-built_release_tag="${gcc_version}-${binutils_version}"
+cilium_revision=$(cat "$BPF_INSTALLDIR/cilium-revision" 2>/dev/null || true)
+built_release_tag="cilium-${cilium_revision}"
 
 if [[ "$latest_release_tag" == "$built_release_tag" ]]; then
     echo "Release with tag $latest_release_tag already exists"
     exit 0
 fi
 
-if [[ -z "$built_release_tag" ]]; then
+if [[ -z "$cilium_revision" ]]; then
     echo "Could not determine built release tag"
     exit 1
 fi
 
-cd $(dirname $(realpath $GCC_BPF_INSTALLDIR))
-tar -I "zstd -19 -T0" -cf ${built_release_tag}.tar.zst $(basename $(realpath $GCC_BPF_INSTALLDIR))
+cd $(dirname $(realpath $BPF_INSTALLDIR))
+tar -I "zstd -19 -T0" -cf ${built_release_tag}.tar.zst $(basename $(realpath $BPF_INSTALLDIR))
 
 # assume access to gh and upload a new release
 gh release create ${built_release_tag} ./${built_release_tag}.tar.zst -p
